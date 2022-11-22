114355479a698a35f8c982eb7460d0aa3cc3f09b
Christian Sandholdt Trip <90309082+ChristianTrip@users.noreply.github.com>
2022-11-22 14:26:22 +0100

    [BS-25] added links to properties filen, created a user entity to test db connectivity

--- src/test/java/couchpotatoes/spurven/SpurvenApplicationTests.java
+++ src/test/java/couchpotatoes/spurven/SpurvenApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpurvenApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
